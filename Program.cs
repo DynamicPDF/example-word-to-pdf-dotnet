e9d904cd426f5c644bcede9c0e8c8bc1142c4cbc
Andrew Cochran <andrew@dynamicpdf.com>
2021-01-13 16:39:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ceTe.DynamicPDF.Conversion;
+
+namespace WordToPdfExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordConversionOptions options = new WordConversionOptions(false);
+            options.Author = "John Doe";
+            options.TopMargin = 144;
+            options.BottomMargin = 72;
+            options.LeftMargin = 72;
+            options.RightMargin = 72;
+
+            WordConverter converter = new WordConverter("../../../resources/sample.docx", options);
+            converter.Convert("sample.pdf");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+﻿﻿# Convert Word to PDF Example for C# (.NET Core)
+
+This example shows how to convert a Word document to PDF. More details can be found
+[here](https://www.dynamicpdf.com/examples/word-to-pdf-.net-core).
